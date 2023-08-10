aaaf08cd24ace8401ba60b7a01a276a6347279d9
Miguel Farias <mike_fa96@hotmail.com>
2023-08-09 20:50:48 -0600

    [Feat] Custom Fonts => Firsts steps

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_social/widgets/list_view.dart';
+import 'package:flutter_social/widgets/custom_font.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MyListView(),
+      home: CustomFont(),
     );
   }
 }
