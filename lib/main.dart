8c6038da74c04f3fb052f8e4a2968f602e6161c6
Miguel Farias <mike_fa96@hotmail.com>
2023-08-09 21:17:04 -0600

    [Feat] Custom Icons => Firsts steps

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_social/widgets/custom_font.dart';
+import 'package:flutter_social/widgets/custom_icons.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: CustomFont(),
+      home: CustomIcon(),
     );
   }
 }
---
+++ lib/widgets/custom_icons.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_social/icons/custom_icons_icons.dart';
+
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Icon(
+          CustomIcons.idea,
+          size: 100,
+          color: Color.fromARGB(255, 12, 115, 199),
+        ),
+      ),
+    );
+  }
+}
